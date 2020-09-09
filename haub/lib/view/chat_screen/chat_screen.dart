a7e58dc8019e8abba3910fef192da0d093682893
Robson Lopes <robson060814@hotmail.com>
2020-09-08 22:48:34 -0300

    testando git e mudança de cor na tab do chat

--- haub/lib/view/chat_screen/chat_screen.dart
+++ haub/lib/view/chat_screen/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:haub/models/colorPalette.dart';
 
 class MyChatPage extends StatelessWidget {
   MyChatPage({Key key}) : super(key: key);
@@ -7,9 +8,9 @@ class MyChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: ColorPalette.primaryColor,
         title: const Text('Haub chat'),
       ),
-      
       body: Center(
         child: Text("Chat screen"),
       ),
