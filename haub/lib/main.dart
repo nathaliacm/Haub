1d062aaac3cb72c841beea1808d0cbc0abcd6f4c
Cecilia Soares <ceciliasoares0@gmail.com>
2020-09-11 17:30:37 -0300

    Adicionado bottom app bar

--- haub/lib/main.dart
+++ haub/lib/main.dart
@@ -13,55 +13,88 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: _title,  
-      home:  MyHomePageWidget(),
+      title: _title,
+      home: MyHomePageWidget(),
     );
   }
 }
 
 class MyHomePageWidget extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: ColorPalette.primaryColor,
-        title: const Text('Haub home'),
-        actions: <Widget>[
-          IconButton(
-            icon: const Icon(Icons.search),
-            onPressed: () {},
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(100.0),
+          child: AppBar(
+            backgroundColor: ColorPalette.primaryColor,
+            title: const Text(
+              'Haub',
+              style: TextStyle(
+                fontSize: 34,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: () {},
+              ),
+            ],
           ),
-        ],
-      ),
-
-      drawer: MyDrawer(),
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            RaisedButton(
-              child: Text("Navegar para o chat"),
-              color: ColorPalette.secondaryColor,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (BuildContext context) => MyChatPage()
-              ));
-            }),
-
-            RaisedButton(
-              child: Text("Navegar para o adicionar"),
-              color: ColorPalette.secondaryColor,
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(
-                  builder: (BuildContext context) => MyQuestionPage()
-              ));
-            }),
-
-          ],
-        ),   
-      ),      
-    );
+        ),
+        drawer: MyDrawer(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                  child: Text("Navegar para o chat"),
+                  color: ColorPalette.secondaryColor,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => MyChatPage()));
+                  }),
+              RaisedButton(
+                  child: Text("Navegar para o adicionar"),
+                  color: ColorPalette.secondaryColor,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                MyQuestionPage()));
+                  }),
+            ],
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: ColorPalette.secondaryColor,
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => MyQuestionPage()));
+          },
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+          elevation: 2.0,
+        ),
+        bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            notchMargin: 7.0,
+            color: ColorPalette.primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(12, 30, 12, 10),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+              ),
+            )));
   }
 }
