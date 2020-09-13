d44de830ab00c1d0dd4f48c681f5c3ed8ea929fc
Thiago Henrique <thiagohhab@gmail.com>
2020-09-13 15:55:15 -0300

    Criando model para telas com dropdown button e model para raisedButton

---
+++ haub/lib/models/dropDownButtomPages.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:haub/models/appBar.dart';
+import 'package:haub/models/raisedButton.dart';
+
+class DropDownPage extends StatefulWidget {
+  final List<String> dropDownList;
+  final String textBeforeBox;
+  final Function onPressedFunction;
+
+  DropDownPage(this.dropDownList, this.textBeforeBox, this.onPressedFunction);
+  @override
+  _DropDownState createState() => _DropDownState();
+}
+
+class _DropDownState extends State<DropDownPage> {
+
+  String nomeAreaProficiencia = "";
+  var _selectedDropDownItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CircularAppBar(" ", 60, true, 20),
+      body: criaDropDownButton(),
+    );
+  }
+
+  criaDropDownButton() {
+    return Container(
+      padding: EdgeInsets.only(top: 150),
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Text(
+              widget.textBeforeBox,
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Colors.grey[300],
+                ),
+                width: 310,
+                child: SingleChildScrollView(
+                  child: DropdownButton<String>(
+                      isExpanded: true,
+                      items: widget.dropDownList.map((String dropDownStringItem) {
+                        return DropdownMenuItem<String>(
+                          value: dropDownStringItem,
+                          child: Padding(
+                            padding: EdgeInsets.only(left: 10),
+                            child: Text(dropDownStringItem),
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String novoItemSelecionado) {
+                        _dropDownItemSelected(novoItemSelecionado);
+                        setState(() {
+                          this._selectedDropDownItem = novoItemSelecionado;
+                        });
+                      },
+                      value: _selectedDropDownItem),
+                ),
+              ),
+            ],
+          ),
+          Container(
+              width: 200,
+              padding: EdgeInsets.only(top: 40),
+              child: MyRaisedButton(widget.onPressedFunction, "Avançar")),
+        ],
+      ),
+    );
+  }
+
+  void _dropDownItemSelected(String novoItem) {
+    setState(() {
+      this._selectedDropDownItem = novoItem;
+    });
+  }
+}
--- haub/lib/view/add_question/doubt_level.dart
+++ haub/lib/view/add_question/doubt_level.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:haub/models/appBar.dart';
-
-import 'doubt_abstract.dart';
+import 'package:haub/models/dropDownButtomPages.dart';
+import 'package:haub/view/add_question/doubt_abstract.dart';
 
 class MyDaubtLevelPage extends StatelessWidget {
   MyDaubtLevelPage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CircularAppBar("Haub Question Level", 60, true, 20),
-      body: Center(
-        child: RaisedButton(
-          child: Text("Navegar para abstract page"),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (BuildContext context) => MyDaubtAbstractPage()
-          ));
-        }),
-      ),
+    return Container(
+      child: DropDownPage(['Doutor', 'Mestre', 'Graduado', 'Técnico', 'Ensino Médio', 'Ensino Fundamental'], 
+          "Selecione o level de sua dúvida",
+          (){Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => MyDaubtAbstractPage()));}
+        ),
     );
   }
 }
