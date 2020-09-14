a0c502c0cf6497c769b00ee2f59acd17791fd83d
rcoelhop <renan.coelho.pinheiro06@aluno.ifce.edu.br>
2020-09-14 15:28:09 -0300

    Ajustes realizados: sincronizadas alterações entre branches

--- haub/lib/view/chat_screen/chat_screen.dart
+++ haub/lib/view/chat_screen/chat_screen.dart
@@ -18,59 +18,61 @@ class MyChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: ColorPalette.primaryColor,
-          title: const Text('Haub chat'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.more_vert),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-              Expanded(
-                child: StreamBuilder<QuerySnapshot>(
-                  stream: FirebaseFirestore.instance.collection('messages').snapshots(),
-                  builder: (context, snapshot) {
-                    switch (snapshot.connectionState) {
-                      case ConnectionState.none:
-                      case ConnectionState.waiting:
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      default:
-                        List<DocumentSnapshot> documents =
-                          snapshot.data.docs.reversed.toList();
-                        return ListView.builder(
-                          itemCount: documents.length,
-                          reverse: true,
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Text(documents[index].data()['text']),
-                            );
-                          }
-                        );
-                    }
-                  },
-                ),
+      appBar: AppBar(
+        backgroundColor: ColorPalette.primaryColor,
+        title: const Text('Haub chat'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('messages').orderBy(
+                  'timestamp', descending:true
+                ).snapshots(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      List<DocumentSnapshot> documents =
+                        snapshot.data.docs.toList();
+                      return ListView.builder(
+                        itemCount: documents.length,
+                        reverse: true,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            title: Text(documents[index].data()['text']),
+                          );
+                        }
+                      );
+                  }
+                },
               ),
-              Center(
-                  child: Ink(
-                      decoration: ShapeDecoration(
-                          shape: RoundedRectangleBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(20.0))),
-                          color: ColorPalette.primaryColor),
-                      child: TextComposer(_sendMessage)
-                  )
-                )
-            ]
-          )
+            ),
+            Center(
+              child: Ink(
+                  decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(20.0))),
+                      color: ColorPalette.primaryColor),
+                  child: TextComposer(_sendMessage)
+              )
+            )
+          ]
         )
-      );
+      )
+    );
   }
 }
