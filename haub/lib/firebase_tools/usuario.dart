5c10d995b5729e7a635d6dd4890feab8f9c9cad8
rcoelhop <renan.coelho.pinheiro06@aluno.ifce.edu.br>
2020-09-16 22:28:59 -0300

    Pequena alteração usuario

--- haub/lib/firebase_tools/usuario.dart
+++ haub/lib/firebase_tools/usuario.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:haub/firebase_tools/conversa.dart';
 import 'package:haub/firebase_tools/duvida.dart';
 
 abstract class Usuario {
